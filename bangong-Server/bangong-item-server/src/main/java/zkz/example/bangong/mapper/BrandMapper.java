9f65bc522853cf4276c9316f470f6372bb42c246
zkz13078229409 <2321361726@qq.com>
2019-10-14 16:58:42 +0800

    微服务1.0
    微服务1.0

---
+++ bangong-Server/bangong-item-server/src/main/java/zkz/example/bangong/mapper/BrandMapper.java
@@ -0,0 +1,7 @@
+package zkz.example.bangong.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import zkz.example.bangong.pojo.Brand;
+
+public interface BrandMapper extends Mapper<Brand> {
+}
---
+++ eureka-Server/src/main/java/zkz/example/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package zkz.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // 声明当前springboot应用是一个eureka服务中心
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
